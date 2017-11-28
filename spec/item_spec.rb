f181b2c6a8d257e1af0d6f511344340d0178a65f
David Halewood <davidnhalewood@hotmail.com>
2017-11-28 12:09:14 +0000

    Adding test for Items quality value

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,11 +6,17 @@ describe Item do
   sell_in = 20
   quality = 10
   let(:item) { Item.new(name, sell_in, quality) }
-  it 'is created with a name' do
-    expect(item.name).to eq 'Foo'
-  end
+  context '#new' do
+    it 'is created with a name' do
+      expect(item.name).to eq 'Foo'
+    end
 
-  it 'is created with a value for the number of days to sell within' do
-    expect(item.sell_in).to eq 20
+    it 'is created with a value for the number of days to sell within' do
+      expect(item.sell_in).to eq 20
+    end
+
+    it 'is created with a value for it\'s quality metric' do
+      expect(item.quality).to eq 10
+    end
   end
 end
