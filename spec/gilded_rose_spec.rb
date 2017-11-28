6b1eb088e015181ee1a08aeaefae4750b5b0bf0b
David Halewood <davidnhalewood@hotmail.com>
2017-11-28 14:40:57 +0000

    Adding test for reduction of quality

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,9 +1,15 @@
 describe GildedRose do
     
-  describe "#update_quality" do
-    it "does not change the quality if quality is 0" do
-      items = [Item.new("Item", 0, 0)]
+  describe '#update_quality' do
+    it 'does not change the quality if quality is 0' do
+      items = [Item.new('Item', 0, 0)]
       GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 0
+    end
+
+    it 'reduces quality by 1 if more than 0' do
+      items = [Item.new('foo', 0, 1)]
+      GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 0
     end
   end
