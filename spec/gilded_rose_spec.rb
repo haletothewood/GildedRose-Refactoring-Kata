2df4e37f371a00eb271cb936df599e83e63a10d2
David Halewood <davidnhalewood@hotmail.com>
2017-11-28 14:59:16 +0000

    Adding tests for Sulfuras

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -41,10 +41,24 @@ describe GildedRose do
       expect(items[0].quality).to eq 2
     end
 
-    it "quality has a max value of 50" do
-      items = [Item.new("Aged Brie", 0, 50)]
+    it 'quality has a max value of 50' do
+      items = [Item.new('Aged Brie', 0, 50)]
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 50
+    end
+  end
+
+  context 'Sulfuras, Hand of Ragnaros' do
+    it 'doesn\'t change quality' do
+      items = [Item.new('Sulfuras, Hand of Ragnaros', 10, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 10
+    end
+
+    it 'doesn\'t change sell in value' do
+      items = [Item.new('Sulfuras, Hand of Ragnaros', 15, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq 15
     end
   end
 end
