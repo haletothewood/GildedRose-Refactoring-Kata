f19c5981d287d3e661cfb9e31e859488cf71dcde
David Halewood <davidnhalewood@hotmail.com>
2017-11-28 12:14:29 +0000

    Adding test for to_s method in Item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -52,4 +52,3 @@ class GildedRose
     end
   end
 end
-
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,7 +6,7 @@ describe Item do
   sell_in = 20
   quality = 10
   let(:item) { Item.new(name, sell_in, quality) }
-  context '#new' do
+  describe '#new' do
     it 'is created with a name' do
       expect(item.name).to eq 'Foo'
     end
@@ -17,6 +17,12 @@ describe Item do
 
     it 'is created with a value for it\'s quality metric' do
       expect(item.quality).to eq 10
+    end
+  end
+
+  describe '#to_string' do
+    it 'converts the items attributes to a string' do
+      expect(item.to_s).to eq "Foo, 20, 10"
     end
   end
 end
