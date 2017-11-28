2693b8d3b9db4cb758f5d53138a24e2a9e645047
David Halewood <davidnhalewood@hotmail.com>
2017-11-28 11:29:01 +0000

    Fixing first test and removing dependancy to item class

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,11 +2,15 @@ require_relative '../lib/gilded_rose.rb'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+  let(:item) { double :item, name: 'foo', sell_in: 0, quality: 0 }
+
+  describe '#update_quality' do
+    it 'does not change the name' do
+      items = [item]
+      allow(item).to receive(:quality=).with(1)
+      allow(item).to receive(:sell_in=).with(-1)
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq 'foo'
     end
   end
 end
