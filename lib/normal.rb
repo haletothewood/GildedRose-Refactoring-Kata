d02cb887b8798bf910d155a3f653cab92a18f984
David Halewood <davidnhalewood@hotmail.com>
2017-11-29 14:19:56 +0000

    Adding update method for normal item

--- lib/normal.rb
+++ lib/normal.rb
@@ -9,5 +9,8 @@ class Normal < Item
 
   def update
     @sell_in -= 1
+    return if @quality <= 0 
+    @quality -= 1
+    @quality -= 1 if @sell_in <= 0 && @quality > 0
   end
 end
--- spec/normal_spec.rb
+++ spec/normal_spec.rb
@@ -3,11 +3,27 @@ require 'normal'
 
 describe Normal do
 
-  let(:normal) { described_class.new('Normal', 10, 20) }
-
   context '#update' do
+    let(:normal) { described_class.new('Normal', 10, 20) }
+    before :each do
+      normal.update
+    end
     it 'reduces the sell in value by one' do
-      expect { normal.update }.to change { normal.sell_in }.by(-1)
+      expect(normal.sell_in).to eq 9
+    end
+
+    it 'does not change the quality if quality is 0' do
+      50.times { normal.update }
+      expect(normal.quality).to eq 0
+    end
+
+    it 'reduces quality by 1 if more than 0' do
+      expect(normal.quality).to eq 19
+    end
+
+    it 'reduces the quality by 2 when sell in is 0 or less' do
+      10.times { normal.update }
+      expect { normal.update }.to change { normal.quality }.by(-2)
     end
   end
 end
