111bc0fae18236eb5ad3d9d62ea5211e01ed4e70
David Halewood <davidnhalewood@hotmail.com>
2017-11-29 16:18:36 +0000

    Refactoring gilded rose to receive items as classes and call update

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,12 +1,6 @@
 class GildedRose
   attr_reader :items
 
-  NAMED_ITEMS = [
-    'Aged Brie',
-    'Backstage passes to a TAFKAL80ETC concert',
-    'Sulfuras, Hand of Ragnaros',
-    'Conjured'
-  ]
   MAXIMUM_QUALITY = 50
   MINIMUM_QUALITY = 0
   SELL_BY_DATE = 0
@@ -20,72 +14,7 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      case item.name
-        when NAMED_ITEMS[0]
-          return brie_update(item)
-        when NAMED_ITEMS[1]
-          return backstage_update(item)
-        when NAMED_ITEMS[2]
-          return sulfuras_update(item)
-        when NAMED_ITEMS[3]
-          return conjured_update(item)
-        else
-          return normal_update(item)
-      end
+      item.update
     end
-  end
-
-  private
-
-  def normal_update(item)
-    reduce_sell_in(item)
-    return unless quality?(item)
-    item.quality -= 1
-    item.quality -= 1 if perished?(item)
-  end
-
-  def brie_update(item)
-    reduce_sell_in(item)
-    return if item.quality >= MAXIMUM_QUALITY
-    item.quality += 1
-    item.quality += 1 if perished?(item)
-  end
-
-  def backstage_update(item)
-    reduce_sell_in(item)
-    item.quality += 1
-    item.quality += 1 if within_ten_days(item)
-    item.quality += 2 if within_five_days(item)
-    item.quality = MINIMUM_QUALITY if perished?(item)
-  end
-
-  def sulfuras_update(item)
-  end
-
-  def conjured_update(item)
-    reduce_sell_in(item)
-    return unless quality?(item)
-    item.quality -= 2
-    item.quality -= 2 if perished?(item)
-  end
-
-  def reduce_sell_in(item)
-    item.sell_in -= ONE_DAY
-  end
-
-  def perished?(item)
-    item.sell_in <= SELL_BY_DATE
-  end
-
-  def quality?(item)
-    item.quality > MINIMUM_QUALITY
-  end
-
-  def within_ten_days(item)
-    item.sell_in <= TEN_DAYS && item.sell_in > FIVE_DAYS
-  end
-
-  def within_five_days(item)
-    item.sell_in <= FIVE_DAYS && !perished?(item)
   end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,124 +1,20 @@
 describe GildedRose do
-    
+  let(:normal) { double :normal, name: 'Normal' }
+  let(:brie) { double :brie, name: 'Aged_Brie' }
+  let(:backstage) { double :backstage, name: 'Backstage passes to a TAFKAL80ETC concert' }
+  let(:sulfuras) { double :sulfuras, name: 'Sulfuras, Hand of Ragnaros' }
+  let(:conjured) { double :conjured, name: 'Conjured' }
+  let(:items) { [normal, brie, backstage, sulfuras, conjured] }
+  let(:gilded_rose) { described_class.new(items)}
+
   describe '#update_quality' do
-    it 'does not change the quality if quality is 0' do
-      items = [Item.new('Item', 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 0
-    end
-
-    it 'reduces quality by 1 if more than 0' do
-      items = [Item.new('Item', 10, 1)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 0
-    end
-
-    it 'reduces sell in value by 1' do
-      items = [Item.new('Item', 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq -1
-    end
-
-    it 'reduces the quality by 2 when sell in is 0 or less' do
-      items = [Item.new('Item', 0, 20)]
-      GildedRose.new(items).update_quality
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq -2
-      expect(items[0].quality).to eq 16
-    end
-  end
-
-  context 'Aged Brie' do
-    it 'reduces sell in value by 1' do
-      items = [Item.new('Aged Brie', 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq -1
-    end
-
-    it 'increases the quality by 1 if sell in value is more than 0' do
-      items = [Item.new('Aged Brie', 10, 1)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 2
-    end
-
-    it 'increases the the quality by 2 if sell in value is 0 or less' do
-      items = [Item.new('Aged Brie', 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 2
-    end
-
-    it 'quality has a max value of 50' do
-      items = [Item.new('Aged Brie', 0, 50)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 50
-    end
-  end
-
-  context 'Sulfuras, Hand of Ragnaros' do
-    it 'doesn\'t change quality' do
-      items = [Item.new('Sulfuras, Hand of Ragnaros', 10, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 10
-    end
-
-    it 'doesn\'t change sell in value' do
-      items = [Item.new('Sulfuras, Hand of Ragnaros', 15, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq 15
-    end
-  end
-
-  context 'Backstage passes to a TAFKAL80ETC concert' do
-    it 'reduces sell in value by 1' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq -1
-    end
-
-    it 'increases the quality by 1 if sell in value is more than 10' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 21, 1)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 2
-    end
-
-    it 'increases quality by 2 when sell in value is less or equal to 10 and more than 5' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 7, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 12
-    end
-
-    it 'increases quality by 3 when sell in value is less or equal to 5 days and more than 0' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 2, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 13
-    end
-
-    it 'sets the quality to 0 if sell in value is 0' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 8)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 0
-    end
-  end
-
-  context 'Conjured' do
-    it 'reduces sell in value by 1' do
-      items = [Item.new('Conjured', 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq -1
-    end
-
-    it 'reduces quality by 2 if sell in value is more than 0' do
-      items = [Item.new('Conjured', 10, 8)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 6
-    end
-
-    it 'reduces the quality by 4 when sell in is 0 or less' do
-      items = [Item.new('Conjured', 0, 20)]
-      GildedRose.new(items).update_quality
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq -2
-      expect(items[0].quality).to eq 12
+    it 'instructs each item to update it\'s quality' do
+      expect(normal).to receive(:update)
+      expect(brie).to receive(:update)
+      expect(backstage).to receive(:update)
+      expect(sulfuras).to receive(:update)
+      expect(conjured).to receive(:update)
+      gilded_rose.update_quality
     end
   end
 end
