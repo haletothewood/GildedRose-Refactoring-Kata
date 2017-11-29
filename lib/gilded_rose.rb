9ac4995e0d292d6d82fad503a87ddfafb39ef4de
David Halewood <davidnhalewood@hotmail.com>
2017-11-29 16:20:29 +0000

    Further refactoring of gilded rose

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,13 +1,5 @@
 class GildedRose
-  attr_reader :items
-
-  MAXIMUM_QUALITY = 50
-  MINIMUM_QUALITY = 0
-  SELL_BY_DATE = 0
-  ONE_DAY = 1
-  TEN_DAYS = 10
-  FIVE_DAYS = 5
-
+  
   def initialize(items)
     @items = items
   end
