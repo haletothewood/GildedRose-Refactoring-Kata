0256f7da41ae6393e8bd030fb6b5f41054c6a8b6
David Halewood <davidnhalewood@hotmail.com>
2017-11-29 14:26:45 +0000

    Refactoring normal item

--- lib/normal.rb
+++ lib/normal.rb
@@ -1,6 +1,9 @@
 class Normal < Item
   attr_reader :name, :sell_in, :quality
 
+  SELL_BY_DATE = 0
+  MINIMUM_QUALITY = 0
+
   def initialize(name, sell_in, quality)
     @name = name
     @sell_in = sell_in
@@ -9,8 +12,18 @@ class Normal < Item
 
   def update
     @sell_in -= 1
-    return if @quality <= 0 
+    return unless quality? 
     @quality -= 1
-    @quality -= 1 if @sell_in <= 0 && @quality > 0
+    @quality -= 1 if perished? && quality?
+  end
+
+  private
+
+  def perished?
+    @sell_in <= SELL_BY_DATE
+  end
+
+  def quality?
+    @quality > MINIMUM_QUALITY
   end
 end
